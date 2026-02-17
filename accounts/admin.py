d374431bb517bbf932f232270d07307dc499949a
Prateek Kulshrestha <prateek@example.com>
2026-02-17 10:56:22 +0530

    Fix admin password change for custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .models import User
 
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "email", "role", "is_staff", "is_active")
-    list_filter = ("role", "is_staff")
-    search_fields = ("email",)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    ordering = ("id",)
+    list_display = ("username", "email", "role", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (_("Permissions"), {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Custom fields"), {"fields": ("role",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "role",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_active",
+            ),
+        }),
+    )
+
+    search_fields = ("username", "email")
