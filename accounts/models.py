0165833947431a18e72a5d1b85d71a73717aa2f5
Prateek Kulshrestha <prateek@example.com>
2026-03-06 14:18:35 +0530

    fix custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
+
     ROLE_CHOICES = (
-        ("ADMIN", "Admin"),
         ("HR", "HR"),
         ("EMPLOYEE", "Employee"),
     )
@@ -11,7 +12,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=ROLE_CHOICES,
-        default="EMPLOYEE",
+        default="EMPLOYEE"
     )
 
     def __str__(self):
